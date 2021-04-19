e308146a92558e1f64e531cacc0302e568e286a5
André <andreglock@gmail.com>
2021-04-19 09:13:41 +0200

    defined i as let

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 let sumTwenty = 0;
 
-for (i = 1; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
     sumTwenty = sumTwenty + i;
 }
 
@@ -16,7 +16,7 @@ for (i = 0; i < 5; i++) {
     }
 }
 
-for (i = 0; i < 21; i++) {
+for (let i = 0; i < 21; i++) {
     if (i % 2 === 0) {
         console.log(`${i} is even.`);
     } else {
@@ -24,13 +24,13 @@ for (i = 0; i < 21; i++) {
     }
 }
 
-for (i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     for (j = 1; j <= 10; j++) {
         console.log(`${i} * ${j} = ${i * j}`);
     }
 }
 
-for (i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz');
     } else if (i % 3 === 0) {
@@ -44,7 +44,7 @@ for (i = 1; i <= 100; i++) {
 
 let sumThousand = 0;
 
-for (i = 0; i < 1000; i++) {
+for (let i = 0; i < 1000; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
         sumThousand = sumThousand + i;
     }
@@ -52,41 +52,41 @@ for (i = 0; i < 1000; i++) {
 
 console.log('6:', sumThousand);
 
-for (i = 1; i <=10; i++) {
+for (let i = 1; i <=10; i++) {
     process.stdout.write(`${i * 100} `);
 }
 console.log('');
 
-for (i = 0; i < 6; i++) {
+for (let i = 0; i < 6; i++) {
     process.stdout.write(`${i * 2} `);
 }
 console.log('');
 
-for (i = 1; i < 6; i++) {
+for (let i = 1; i < 6; i++) {
     process.stdout.write(`${i * 3} `);
 }
 console.log('');
 
-for (i = 9; i >= 0; i--) {
+for (let i = 9; i >= 0; i--) {
     process.stdout.write(`${i} `);
 }
 console.log('');
 
-for (i = 1; i < 5; i++) {
+for (let i = 1; i < 5; i++) {
     process.stdout.write(`${i} ${i} ${i} `);
 }
 console.log('');
 
-for (i = 1; i < 5; i++) {
+for (let i = 1; i < 5; i++) {
     process.stdout.write(`${i} ${i} ${i} `);
 }
 console.log('');
 
-for (i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     process.stdout.write(`${i} `);
-} for (i = 0; i < 5; i++) {
+} for (let i = 0; i < 5; i++) {
     process.stdout.write(`${i} `);
-} for (i = 0; i < 5; i++) {
+} for (let i = 0; i < 5; i++) {
     process.stdout.write(`${i} `);
 }
 console.log('');
@@ -95,7 +95,7 @@ const wordPalindrome = 'tarrarrat';
 
 let wordArray = wordPalindrome.split("");
 
-for (i = 0; i < wordArray.length / 2; i++) {
+for (let i = 0; i < wordArray.length / 2; i++) {
     if (wordArray[i] !== wordArray[wordArray.length - i - 1]) {
         console.log(wordArray[i], wordArray[wordArray.length - i]);
         console.log('Word is not a palindrome');
